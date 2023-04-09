66fc68a82f226d5e8e8d5a8fa7047904d50815ed
ram-repo <ramjagadeesh2205@gmail.com>
2023-04-09 11:40:36 +0530

    Create sample.groovy

---
+++ vars/sample.groovy
@@ -0,0 +1,47 @@
+Library('common-lib') _
+  
+def call (){
+properties([
+    parameters([
+        //choice(name: 'projectsview', description: 'Please pick one', choices: ['ramkoti', 'BRESI', 'test', 'prod']),
+        string(name: 'Name', description: 'projectsview ', defaultValue: 'Admin'),
+        string(name: 'Name', description: 'RepoName ', defaultValue: 'game-of-life'),
+        //string(name: 'Name', description: 'projectsview' , defaultValue: 'test')
+        booleanParam(defaultValue: true, description: 'to create multibranch pipeline', name: 'create'),
+        booleanParam(description: 'to dellete the job', name: 'deletejob'),
+        booleanParam(description: 'create folder', name: 'folder')
+    ])
+])
+
+
+pipeline {
+    agent any
+    stages {
+        stage('Create MutiBranch pipline') {
+            when { expression {return params.create } }
+            steps {
+                script {
+                   echo "$params.projectsview $params.Name"
+                    }
+                }
+            }
+          stage('Delete MutiBranch pipline') {
+              when { expression {return params.deletejob } }
+            steps {
+                script {
+                   echo "$params.projectsview $params.Name"
+                    }
+                }
+            }
+
+          stage('createNewJenkinsFolder') {
+               when { expression {return params.folder } }
+              steps {
+                script {
+                    echo "$params.projectsview $params.Name"
+                }
+            }
+        }
+      }
+    }
+}
