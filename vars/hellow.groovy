d5f90dc3726406a1f69e1d0513945237ccf5e27b
ram-repo <ramjagadeesh2205@gmail.com>
2023-04-09 15:11:48 +0530

    Create hellow.groovy

---
+++ vars/hellow.groovy
@@ -0,0 +1,13 @@
+def call() {
+  pipeline {
+      agent any
+
+      stages {
+          stage('Hello') {
+              steps {
+                  echo 'Hello World'
+              }
+          }
+      }
+  }
+}
